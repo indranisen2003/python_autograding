b174ed67ee00acffce4077bbd0f1ef072dbeacc5
indranisen2003 <46016997+indranisen2003@users.noreply.github.com>
2021-09-19 22:04:44 +0530

    Update hello_test.py

--- hello_test.py
+++ hello_test.py
@@ -1,8 +1,8 @@
 
 
-try:
-  hello_world()
-except:
-  print ("An exception was thrown!")
-  
-  assert False, f"raised an exception"
+def exec():
+    try:
+        hello_world()
+    except:
+        print ("An exception was thrown!")
+        assert False, f"raised an exception"
